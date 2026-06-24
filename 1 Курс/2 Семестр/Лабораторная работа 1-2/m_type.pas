e94665528847092e47625dbdfbc0c910d4a3c183
pyanov.vd <v-pyanov@bk.ru>
2026-06-24 16:35:38 +1000

    Добавлены все текущие файлы

---
+++ 1 Курс/2 Семестр/Лабораторная работа 1-2/m_type.pas
@@ -0,0 +1,33 @@
+﻿unit m_type;
+
+interface
+type
+  snils = record
+    number: string;
+    ks: word;
+  end;
+
+  date = record
+    dd: word;
+    mm: word;
+    yy: word;
+  end;
+
+  abiturient = record
+    snils: snils;
+    date_bir: date;
+    prikaz: string;
+    date_prikaz: date;
+    kod_post: string;
+    sum_ege: word;
+  end;
+
+const
+  MAX_N = 10;
+
+type
+  abiturienty = array[1..MAX_N] of abiturient;
+implementation
+begin
+ 
+end.
